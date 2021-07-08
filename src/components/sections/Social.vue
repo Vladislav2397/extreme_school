bdc333308f67e50b27d058d6b3ff9da0eac81712
GlaDiatoRs_notebook <valdiszz53@gmail.com>
2021-07-08 08:17:57 +0300

    Create toolUI files and add bem script

---
+++ src/components/sections/Social.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+    include ../../tools/mixins
+    +b.social
+    
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+
+@Component
+export default class Social extends Vue {
+
+}
+
+</script>
