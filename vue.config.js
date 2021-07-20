d5290c50b48699ec82888499cd6ed32bcd01f39e
GlaDiatoRs_notebook <valdiszz53@gmail.com>
2021-07-20 08:25:21 +0300

    update builder

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+/**
+ * Settings for vue
+ * @type { import('@vue/cli-service').ProjectOptions }
+ */
 module.exports = {
     pluginOptions: {
         webpackBundleAnalyzer: {
@@ -12,37 +16,35 @@ module.exports = {
             splitChunks: {
                 chunks: 'all',
                 cacheGroups: {
-                    // debugVendor: {
-                    //     test(module) {
-                    //         return /[\\/]node_modules[\\/](vue-class-component)[\\/]/.test(module.resource)
-                    //     },
-                    //     name: 'debug-vendor'
+                    debugVendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: 'vendor'
+                    },
+                    // vueVuexVendor: {
+                    //     test: /[\\/]node_modules[\\/](vue|vuex|vue-router)[\\/]/,
+                    //     name: 'vue-vuex-vendor',
+                    //     enforce: true
                     // },
-                    vueVuexVendor: {
-                        test: /[\\/]node_modules[\\/](vue|vuex|vue-router)[\\/]/,
-                        name: 'vue-vuex-vendor',
-                        enforce: true
-                    },
-                    vueUtilsVendor: {
-                        test: /[\\/]node_modules[\\/](vue-property-decorator|vue-class-component|vue-lazyload)[\\/]/,
-                        name: 'vue-utils-vendor',
-                        enforce: true
-                    },
-                    vueSwiperVendor: {
-                        test: /[\\/]node_modules[\\/](vue-awesome-swiper)[\\/]/,
-                        name: 'vue-awesome-swiper-vendor',
-                        enforce: true
-                    },
-                    swiperVendor: {
-                        test: /[\\/]node_modules[\\/](swiper)[\\/]/,
-                        name: 'swiper-vendor',
-                        enforce: true
-                    },
-                    utilsVendor: {
-                        test: /[\\/]node_modules[\\/](es6-promise|vue-class-component|vue-lazyload)[\\/]/,
-                        name: 'utils-vendor',
-                        enforce: true
-                    }
+                    // vueUtilsVendor: {
+                    //     test: /[\\/]node_modules[\\/](vue-property-decorator|vue-class-component|vue-lazyload)[\\/]/,
+                    //     name: 'vue-utils-vendor',
+                    //     enforce: true
+                    // },
+                    // vueSwiperVendor: {
+                    //     test: /[\\/]node_modules[\\/](vue-awesome-swiper)[\\/]/,
+                    //     name: 'vue-awesome-swiper-vendor',
+                    //     enforce: true
+                    // },
+                    // swiperVendor: {
+                    //     test: /[\\/]node_modules[\\/](swiper)[\\/]/,
+                    //     name: 'swiper-vendor',
+                    //     enforce: true
+                    // },
+                    // utilsVendor: {
+                    //     test: /[\\/]node_modules[\\/](es6-promise|vue-class-component|vue-lazyload)[\\/]/,
+                    //     name: 'utils-vendor',
+                    //     enforce: true
+                    // }
                 }
             }
         }
